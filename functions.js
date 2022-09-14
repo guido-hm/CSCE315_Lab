b8a204ce493bc094bda89305b93611bd797456cb
Guido Herrera <guidohm@tamu.edu>
2022-09-13 22:09:03 -0500

    worked on second style

---
+++ functions.js
@@ -0,0 +1,23 @@
+function load_style()
+{
+	page_style = localStorage.getItem("page_stylesheet_name", "style1.css");
+	if (page_style === null)
+	{
+		page_style = "style1.css";
+	}
+	document.getElementById('page_style').setAttribute("href", page_style);
+}
+
+function style_1()
+{
+    localStorage.setItem("page_stylesheet_name", "style1.css");
+	load_style();
+}
+
+function style_2()
+{
+    localStorage.setItem("page_stylesheet_name", "style2.css");
+	load_style();
+}
+
+load_style()  // load style immediately
